01b299094c1c7182ab6a7d8dc1ed06f19167c659
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-12-13 19:53:16 +0100

    changing js

---
+++ dragg.js
@@ -0,0 +1,54 @@
+var remove = document.querySelector('.draggable');
+
+function dragStart(e) {
+    this.style.opacity = '0.4';
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+};
+
+function dragEnter(e) {
+    this.classList.add('over');
+}
+
+function dragLeave(e) {
+    e.stopPropagation();
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+}
+
+function dragDrop(e) {
+    if (dragSrcEl != this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    return false;
+}
+
+function dragEnd(e) {
+    var listItens = document.querySelectorAll('.draggable');
+    [].forEach.call(listItens, function (item) {
+        item.classList.remove('over');
+    });
+    this.style.opacity = '1';
+}
+
+function addEventsDragAndDrop(el) {
+    el.addEventListener('dragstart', dragStart, false);
+    el.addEventListener('dragenter', dragEnter, false);
+    el.addEventListener('dragover', dragOver, false);
+    el.addEventListener('dragleave', dragLeave, false);
+    el.addEventListener('drop', dragDrop, false);
+    el.addEventListener('dragend', dragEnd, false);
+}
+
+var listItens = document.querySelectorAll('.draggable');
+[].forEach.call(listItens, function (item) {
+    addEventsDragAndDrop(item);
+});
+
--- index.js
+++ index.js
@@ -1,50 +1,119 @@
-function myFunction(e) {
-    var elems = document.querySelectorAll(".active");
-    [].forEach.call(elems, function (el) {
-        el.classList.remove("active");
-        el.classList.add("paragraf");
-    });
-    e.target.className = "active";
+
+var paragraf = document.querySelectorAll(".paragraf");
+Array.from(paragraf).forEach(item => {
+  item.addEventListener("click", () => {
+    var selected = document.getElementsByClassName("active");
+    selected[0].className = selected[0].className.replace(" active", "");
+    item.className += " active";
+  });
+})
+
+var elements = document.querySelectorAll(".button-cheked");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function () {
+    elements[i].classList.add("check");
+  });
 }
+
 
 var input = document.getElementById("myInput");
 
 input.addEventListener("keyup", function (event) {
-    var x;
-    x = document.getElementById("myInput").value;
-    if (x == "") {
-        alert("Enter  todo  list  ");
-    }
-    if (event.keyCode === 13 && x != "") {
-        event.preventDefault();
-        var row = document.getElementById("myRow");
-        var table = document.getElementById("table");
-        var input = document.getElementById("myInput");
+  var x;
+  x = document.getElementById("myInput").value;
+  if (x == "") {
+    console.log("Enter  todo  list  ");
+  }
+  if (event.keyCode === 13 && x != "") {
+    event.preventDefault();
+    var input = document.getElementById("myInput");
+    const target = document.querySelector('.todo-items');
+    const newTodo = document.createElement('div', draggable = "true");
+    newTodo.className = 'todo-item', 'draggable';
+    newTodo.draggable = "true";
+    target.appendChild(newTodo);
+    target.parentNode.insertBefore(newTodo, target);
+    newTodo.innerHTML = `
+     <div class="button-cheked" onclick="checkText() ">
+  <img class="cheked-btn" 
+    src="https://raw.githubusercontent.com/zaklinaradivojevic/todo-app/8c812e715ad10f19665a13ecd44d1a5d51d6f0be/images/icon-check.svg"
+    alt="cheked-icon">
+</div>
+<div class="todo-text">
+  <p class="todoText" id="0">${input.value}<p>
+</div>
+<div class="delete">
+<img class="delete-row"
+  src="https://raw.githubusercontent.com/zaklinaradivojevic/todo-app/c533248264a38016caf370e258467a7653b41095/images/icon-cross.svg">
+</div>
 
-        var row = table.insertRow(0);
-        var cell = row.insertCell(0);
-        cell.innerHTML = `<div class="table-fild">
-        <div class="table-cheked" id="tablecheked" onclick="checkImg()">
-          <img class="cheked-img"
-            src="https://raw.githubusercontent.com/zaklinaradivojevic/todo-app/8c812e715ad10f19665a13ecd44d1a5d51d6f0be/images/icon-check.svg"
-            alt="cheked-icon">
-           </div>
-            <p id="tableText">${input.value}</p>
-            <img class="delete-row" id="deleteRow" src="https://raw.githubusercontent.com/zaklinaradivojevic/todo-app/c533248264a38016caf370e258467a7653b41095/images/icon-cross.svg" onclick="deleteRow()">
-   ` ;
+`;
+  }
+  var newTodo = document.querySelectorAll(".button-cheked");
+  for (let i = 0; i < newTodo.length; i++) {
+    newTodo[i].addEventListener("click", function () {
+      newTodo[i].classList.toggle("check");
+
+    });
+
+  }
+  var close = document.getElementsByClassName("delete");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
+  }
+
+
+  input.value = "";
+
 });
 
 
 
-function deleteRow() {
-    document.getElementById("table").deleteRow(0);
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("delete");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
-function checkImg() {
-    document.getElementById("tablecheked").classList.toggle("checked-bg");
-    document.getElementById("tableText").classList.toggle("checked-bg-text");
-
+function checkText() {
+  var textDone = document.querySelector(".todoText");
+  textDone.classList.add("checkText");
 }
 
+function checkText1() {
+  var textDone = document.getElementById("1");
+  textDone.classList.add("checkText");
+}
 
+function checkText2() {
+  var textDone = document.getElementById("2");
+  textDone.classList.add("checkText");
+}
+
+function checkText3() {
+  var textDone = document.getElementById("3");
+  textDone.classList.add("checkText");
+}
+
+function checkText4() {
+  var textDone = document.getElementById("4");
+  textDone.classList.add("checkText");
+}
+
+function checkText5() {
+  var textDone = document.getElementById("5");
+  textDone.classList.add("checkText");
+}
+
+function checkText6() {
+  var textDone = document.getElementById("6");
+  textDone.classList.add("checkText");
+}
